02e35b6152cb67e6f8ace65025cd060d3ddb7d6a
yongjun <yes1312@naver.com>
2021-05-06 02:54:58 +0900

    part2

---
+++ Part2 개론 환경 설정/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Part2_개론_환경_설정
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 모든 게임은 입력 -> 로직 -> 렌더링 단계를 거침
+            // FPS 프레임 (프레임이 높을수록 게임이 부드럽게 표현됨)
+            // 프레임이란 1초에 Loop가 몇번 수행되는지를 의미함
+            Console.CursorVisible = false;
+            const int WAIT_TICK = 1000 / 30;
+            const char CIRCLE = '\u25cf';
+
+            int lastTick = 0;
+            while (true)
+            {
+                #region 프레임 관리
+                int currentTick = System.Environment.TickCount;
+                if (currentTick - lastTick < WAIT_TICK)
+                    continue;
+                lastTick = currentTick;
+                #endregion
+
+                Console.SetCursorPosition(0, 0);
+                Console.ForegroundColor = ConsoleColor.Green;
+                for (int i = 0; i < 25; i++)
+                {
+                    for (int j = 0; j < 25; j++)
+                    {
+                        Console.Write(CIRCLE);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ Part2 선형 자료 기초 배열, 동적 할당, 연결리스트 비교/Board.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Part2_선형_자료_기초_배열__동적_할당__연결리스트_비교
+{
+    class Board
+    {
+        //아래 3개 중에 어떤 것을 사용해야할까??
+        //
+        public int[] _data = new int[25];
+        public List<int> _data2 = new List<int>();
+        public LinkedList<int> _data3 = new LinkedList<int>();
+
+        public void Initialize()
+        {
+
+        }
+    }
+}
