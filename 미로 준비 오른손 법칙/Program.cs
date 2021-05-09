421a791764622f799c64fdcff33ecbb5021dfd50
yongjun <yes1312@naver.com>
2021-05-09 17:58:40 +0900

    미로 준비 - 오른손 법칙

---
+++ 미로 준비 오른손 법칙/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace 미로_준비_오른손_법칙
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board board = new Board();
+            Player player = new Player();
+
+            board.Initialize(25, player);
+            player.Initialize(1, 1, board);
+
+            Console.CursorVisible = false;
+            const int WAIT_TICK = 1000 / 30;
+
+            int lastTick = 0;
+            while (true)
+            {
+                #region 프레임 관리
+                int currentTick = System.Environment.TickCount;
+                int deltaTick = currentTick - lastTick;
+                if (deltaTick < WAIT_TICK)
+                    continue;
+                lastTick = currentTick;
+                #endregion
+
+                // 입력
+
+                // 로직
+                player.Update(deltaTick);
+
+                // 렌더링
+                Console.SetCursorPosition(0, 0);
+                board.Render();
+            }
+        }
+
+    }
+}
