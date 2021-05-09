f693e098b8d1e9a42ae2d137dfc483a94e5ac1df
yongjun <yes1312@naver.com>
2021-05-09 16:37:20 +0900

    미로 준비 - 맵 만들기

---
+++ 미로 준비 맵 만들기/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace 미로_준비_맵_만들기
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board board = new Board();
+            board.Initialize(25);
+
+            Console.CursorVisible = false;
+            const int WAIT_TICK = 1000 / 30;
+
+            int lastTick = 0;
+            while (true)
+            {
+                #region 프레임 관리
+                int currentTick = System.Environment.TickCount;
+                if (currentTick - lastTick < WAIT_TICK)
+                    continue;
+                lastTick = currentTick;
+                #endregion
+
+                Console.SetCursorPosition(0, 0);
+                board.Render();
+            }
+        }
+    }
+}
