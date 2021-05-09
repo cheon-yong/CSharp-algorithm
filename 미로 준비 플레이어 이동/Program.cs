f633878e4292ba172584074e91dd64d6c840be33
yongjun <yes1312@naver.com>
2021-05-09 17:28:53 +0900

    미로 준비 - 플레이어 이동

---
+++ 미로 준비 플레이어 이동/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace 미로_준비_플레이어_이동
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board board = new Board();
+            Player player = new Player();
+            
+            board.Initialize(25, player);
+            player.Initialize(1, 1, board.Size - 2, board.Size - 2, board);
+
+            Console.CursorVisible = false;
+            const int WAIT_TICK = 1000 / 30;
+
+            int lastTick = 0;
+            while (true)
+            {
+                #region 프레임 관리
+                int currentTick = System.Environment.TickCount;
+                int deltaTick = currentTick - lastTick;
+                if (deltaTick < WAIT_TICK)
+                    continue;
+                lastTick = currentTick;
+                #endregion
+
+                // 입력
+
+                // 로직
+                player.Update(deltaTick);
+
+                // 렌더링
+                Console.SetCursorPosition(0, 0);
+                board.Render();
+            }
+        }
+    }
+}
