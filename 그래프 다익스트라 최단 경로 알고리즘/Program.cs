9fccfef968f282e8e9da3c74af503d6d12e6bafe
yongjun <yes1312@naver.com>
2021-05-12 14:46:40 +0900

    트리 - 트리 구현 연습

---
+++ 그래프 다익스트라 최단 경로 알고리즘/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace 그래프_다익스트라_최단_경로_알고리즘
+{
+    class Graph
+    {
+        int[,] adj = new int[6, 6]
+        {
+            { -1, 15, -1, 35, -1, -1 },
+            { 15, -1, 05, 10, -1, -1 },
+            { -1, 05, -1, -1, -1, -1 },
+            { 35, 10, -1, -1, 05, -1 },
+            { -1, -1, -1, 05, -1, 05 },
+            { -1, -1, -1, -1, 05, -1 },
+        };
+
+        public void MyDijikstra(int start)
+        {
+            bool[] visited = new bool[6];
+            int[] distance = new int[6];
+            int[] parent = new int[6];
+
+            Array.Fill(distance, Int32.MaxValue);
+
+            distance[start] = 0;
+            parent[start] = start;
+            
+            while (true)
+            {
+                int closest = Int32.MaxValue;
+                int now = -1;
+                for (int i = 0; i < 6; i++)
+                {
+                    if (visited[i])
+                        continue;
+                    if (distance[i] == Int32.MaxValue || distance[i] >= closest)
+                        continue;
+
+                    closest = distance[i];
+                    now = i;
+                }
+
+                if (now == -1)
+                    break;
+
+                visited[now] = true;
+
+                for (int next = 0; next < 6; next++)
+                {
+                    if (adj[now, next] == -1)
+                        continue;
+                    if (visited[next])
+                        continue;
+                    int nextDist = distance[now] + adj[now, next];
+                    if (nextDist < distance[next])
+                    {
+                        distance[next] = nextDist;
+                        parent[next] = now;
+                    }
+                }
+            }
+        }
+
+        public void Dijikstra(int start)
+        {
+            bool[] visited = new bool[6];
+            int[] distance = new int[6];
+            int[] parent = new int[6];
+
+            Array.Fill(distance, Int32.MaxValue);
+            parent[start] = start;
+            distance[start] = 0;
+
+            while (true)
+            {
+                int closest = Int32.MaxValue;
+                int now = -1;
+
+                for (int i=0; i<6; i++)
+                {
+                    if (visited[i])
+                        continue;
+                    if (distance[i] == Int32.MaxValue || distance[i] >= closest)
+                        continue;
+
+                    closest = distance[i];
+                    now = i;
+                }
+
+                if (now == -1)
+                    break;
+
+                visited[now] = true;
+
+                for (int next = 0; next < 6; next++)
+                {
+                    if (adj[now, next] == -1)
+                        continue;
+                    if (visited[next])
+                        continue;
+
+                    int nextDist = distance[now] + adj[now, next];
+                    if (nextDist < distance[next])
+                    {
+                        distance[next] = nextDist;
+                        parent[next] = now;
+                    }
+                }
+            }
+        }
+
+        public void BFS(int start)
+        {
+            bool[] found = new bool[6];
+            int[] parent = new int[6];
+            int[] distance = new int[6];
+
+            Queue<int> q = new Queue<int>();
+            q.Enqueue(start);
+            found[start] = true;
+            parent[start] = start;
+            distance[start] = 0;
+
+            while (q.Count > 0)
+            {
+                int now = q.Dequeue();
+                Console.WriteLine(now);
+
+                for (int next = 0; next < 6; next++)
+                {
+                    if (adj[now, next] == 0)
+                        continue;
+                    if (found[next])
+                        continue;
+                    q.Enqueue(next);
+                    found[next] = true;
+                    parent[next] = now;
+                    distance[next] = distance[now] + 1;
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // DFS (Depth First Search 깊이 우선 탐색)
+            // BFS (Breadth First Search 너비 우선 탐색)
+            Graph graph = new Graph();
+            graph.Dijikstra(0);
+        }
+    }
+}
---
+++ 트리 트리 구현 연습/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace 트리_트리_구현_연습
+{
+    class TreeNode<T>
+    {
+        public T Data { get; set; }
+        public List<TreeNode<T>> Childen { get; set; } = new List<TreeNode<T>>();
+    }
+    class Program
+    {
+        static TreeNode<string> MakeTree()
+        {
+            TreeNode<string> root = new TreeNode<string>() { Data = "R1 개발실" };
+            {
+                {
+                    TreeNode<string> node = new TreeNode<string>() { Data = "디자인팀" };
+                    node.Childen.Add(new TreeNode<string>() { Data = "전투" });
+                    node.Childen.Add(new TreeNode<string>() { Data = "경제" });
+                    node.Childen.Add(new TreeNode<string>() { Data = "스토리" });
+                    root.Childen.Add(node);
+                }
+                {
+                    TreeNode<string> node = new TreeNode<string>() { Data = "프로그래밍팀" };
+                    node.Childen.Add(new TreeNode<string>() { Data = "서버" });
+                    node.Childen.Add(new TreeNode<string>() { Data = "클라" });
+                    node.Childen.Add(new TreeNode<string>() { Data = "엔진" });
+                    root.Childen.Add(node);
+                }
+                {
+                    TreeNode<string> node = new TreeNode<string>() { Data = "아트팀" };
+                    node.Childen.Add(new TreeNode<string>() { Data = "배경" });
+                    node.Childen.Add(new TreeNode<string>() { Data = "캐릭터" });
+                    root.Childen.Add(node);
+                }
+            }
+            return root;
+        }
+
+        static void PrintTree(TreeNode<string> root)
+        {
+            Console.WriteLine(root.Data);
+
+            foreach (TreeNode<string> child in root.Childen)
+                PrintTree(child);
+        }
+
+        static int GetHeight(TreeNode<string> root)
+        {
+            int height = 0;
+
+            foreach (TreeNode<string> child in root.Childen)
+            {
+                int newHeight = GetHeight(child) + 1;
+                height = Math.Max(height, newHeight);
+            }
+
+            return height;
+        }
+
+        static void Main(string[] args)
+        {
+            TreeNode<string> root = MakeTree();
+            PrintTree(root);
+            Console.WriteLine(GetHeight(root));
+        }
+    }
+}
